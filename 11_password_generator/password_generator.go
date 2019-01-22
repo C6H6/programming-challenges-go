d2ad7536eccc51440c1a49f410a495c2b91e95c6
C6H6 <c3c3h3h3@gmail.com>
2019-01-22 22:20:07 +0100

    password_generator

---
+++ 11_password_generator/password_generator.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
+
+func main() {
+	var lengthPtr = flag.Int("length", 8, "Password length")
+	flag.Parse()
+	var charsets = []string{"abcdefghijklmnopqrstuvwxyz", "ABCDEFGHIJKLMNOPQRSTUVWXYZ", "0123456789", "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~"}
+	var groups = getGroups(*lengthPtr, len(charsets))
+
+	var result = make([]byte, *lengthPtr, *lengthPtr)
+
+	var pos = 0
+	for index, size := range groups {
+		for i := 0; i < size; i++ {
+			result[pos] = getRandomByteFromString(charsets[index])
+			pos++
+		}
+	}
+
+	result = shuffle(result)
+	fmt.Println(string(result))
+}
+
+func getGroups(length int, groups int) []int {
+	var result = make([]int, groups, groups)
+	for i := 0; i < len(result); i++ {
+		result[i] = (length + groups - i - 1) / (groups - i)
+		length -= result[i]
+	}
+
+	return result
+}
+
+func getRandomByteFromString(str string) byte {
+	return str[rand.Intn(len(str))]
+}
+
+func shuffle(arr []byte) []byte {
+	for i := range arr {
+		n := rand.Intn(i + 1)
+		arr[i], arr[n] = arr[n], arr[i]
+	}
+
+	return arr
+}
